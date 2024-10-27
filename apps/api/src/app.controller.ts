9fac805b8072544ac579def3a34d2db1a1a9fa1e
Soheil Ghanbary <soli.ghanbary@gmail.com>
2024-10-26 19:45:06 -0700

    add prisma

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,10 @@ export class AppController {
   @Get('/hello')
   getHello(): { message: string } {
     return this.appService.getHello();
+  }
+
+  @Get('/users')
+  getUsers(): Promise<User[]> {
+    return this.appService.getUsers();
   }
 }
--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { User } from '@prisma/client';
 
 @Injectable()
 export class AppService {
+  constructor(private prisma: PrismaService) {}
+
   getHello(): { message: string } {
     return { message: 'Hello World! from API' };
+  }
+
+  getUsers(): Promise<User[]> {
+    return this.prisma.user.findMany();
   }
 }
